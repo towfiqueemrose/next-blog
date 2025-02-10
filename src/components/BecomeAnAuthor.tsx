4e4c864b93b1761f5cb95bf2674130cd46ebd7d4
towfiqueemrose <110231363+towfiqueemrose@users.noreply.github.com>
2025-02-10 15:02:04 +0600

    Update BecomeAnAuthor.tsx

--- src/components/BecomeAnAuthor.tsx
+++ src/components/BecomeAnAuthor.tsx
@@ -1,21 +1,34 @@
-
 import Image from "next/image";
 import Link from "next/link";
+import { auth } from "@/auth";
 
-export default function BecomeAnAuthor() {
+export default async function BecomeAnAuthor() {
+    const session = await auth();
+
     return (
         <div className="mt-10">
             <div className="create-author-main">
-            <div className="w-full md:w-1/2 mt-10">
+                <div className="w-full md:w-1/2 mt-10">
                     <h3 className="h3">Supper change your planning powers</h3>
                     <h1 className="h1">Become an author and share your great stories 🎉</h1>
-                    <p className="p-lg">Become an author you can earn extra income by writing articles. Read and share new perspectives on just about any topic. Everyone’s welcome.</p>
-                    <Link href="/auth/signin" className="create-author-btn">Become an author</Link>
+                    <p className="p-lg">Become an author you can earn extra income by writing articles. Read and share new perspectives on just about any topic. Everyone's welcome.</p>
+                    
+                    {!session?.user && (
+                        <Link href="/auth/signin" className="create-author-btn">
+                            Become an author
+                        </Link>
+                    )}
                 </div>
                 <div className="w-full md:w-1/2">
-                    <Image src="/becomeAuthor.webp" alt="auth-cov" width={600} height={600} />
+                    <Image 
+                        src="/becomeAuthor.webp" 
+                        alt="auth-cov" 
+                        width={600} 
+                        height={600} 
+                        priority 
+                    />
                 </div>
             </div>
         </div>
-    )
+    );
 }
